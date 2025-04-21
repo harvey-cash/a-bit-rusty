fa8f7046256fb2531111b6b3d3bd937cd4612056
Harvey Cash <harveycash@outlook.com>
2025-04-21 21:59:32 +0100

    Transpiled recursive trace algorithm

--- src/chip/tests/test_trace.rs
+++ src/chip/tests/test_trace.rs
@@ -9,44 +9,53 @@
 // [ ] A Trace has >=1 TraceSegments.
 // [ ] Traces have a state value which defaults to 0.
 
-//GivenSingleTraceSegment_WhenDeleteSinglePoint_ThenStillSingleTrace
-//GivenSingleTraceSegment_WhenDeleteBothPoints_ThenNoTraces
-//GivenTwoTraceSegmentsOverlap_ThenOneTraceExists
-//GivenTwoTraceSegmentsOverlap_WhenDeletePointOfOverlap_ThenTwoTracesExist
-//GivenTwoTraces_ThenTwoTracesExist
-//GiveTwoTraces_WhenJoined_ThenOneExists
-//GivenTwoTracesOnDifferentLayers_ThenTwoTracesExist
-//GivenTwoTracesOnDifferentLayers_WhenJoinedByVia_ThenOneTraceExists
-//GivenTwoTracesJoinedByVia_WhenViaDeleted_ThenTwoTracesExist
 
 use nalgebra::vector;
 
 use crate::chip::TraceMap;
 
 #[test]
-fn given_no_traces_then_no_trace_graph() {
+fn given_no_segments_then_no_traces() {
     let map = TraceMap::new();
     assert_eq!(map.get_graphs().len(), 0);
 }
 
 #[test]
 #[should_panic]
-fn given_trace_ends_too_far_apart_then_panics() {
+fn given_segment_ends_too_far_apart_then_panics() {
     let mut map = TraceMap::new();
     map.add(vector![0, 0, 0], vector![2, 0, 0]);
 }
 
 #[test]
-fn given_one_point_then_one_trace_graph() {
+fn given_one_point_then_one_trace() {
     let mut map = TraceMap::new();
     map.add(vector![0, 0, 0], vector![0, 0, 0]);
     assert_eq!(map.get_graphs().len(), 1);
 }
 
 #[test]
-fn given_two_separate_points_then_two_trace_graphs() {
+fn given_two_separate_points_then_two_traces() {
     let mut map = TraceMap::new();
     map.add(vector!(0, 0, 0), vector!(0, 0, 0));
     map.add(vector!(1, 1, 1), vector!(1, 1, 1));
     assert_eq!(map.get_graphs().len(), 2);
 }
+
+#[test]
+fn given_two_joined_segments_then_one_trace() {
+    let mut map = TraceMap::new();
+    map.add(vector!(0, 0, 0), vector!(0, 0, 0));
+    map.add(vector!(0, 0, 0), vector!(0, 0, 1));
+    assert_eq!(map.get_graphs().len(), 1);
+}
+
+
+//GivenSingleTraceSegment_WhenDeleteSinglePoint_ThenStillSingleTrace
+//GivenSingleTraceSegment_WhenDeleteBothPoints_ThenNoTraces
+//GivenTwoTraceSegmentsOverlap_ThenOneTraceExists
+//GivenTwoTraceSegmentsOverlap_WhenDeletePointOfOverlap_ThenTwoTracesExist
+//GiveTwoTraces_WhenJoined_ThenOneExists
+//GivenTwoTracesOnDifferentLayers_ThenTwoTracesExist
+//GivenTwoTracesOnDifferentLayers_WhenJoinedByVia_ThenOneTraceExists
+//GivenTwoTracesJoinedByVia_WhenViaDeleted_ThenTwoTracesExist
--- src/chip/trace.rs
+++ src/chip/trace.rs
@@ -1,13 +1,76 @@
+use std::collections::{HashMap, HashSet};
+
 use nalgebra::{self, Matrix};
 
 pub type Vector3 = nalgebra::Vector3<i32>;
 
-struct TraceSegment {}
+pub struct TraceMap {
+    trace_map: HashMap<Vector3, HashSet<Vector3>>,
+}
 
-impl TraceSegment {
-    pub fn new(a: Vector3, b: Vector3) -> Self {
+impl TraceMap {
+    pub fn new() -> Self {
+        let trace_map: HashMap<Vector3, HashSet<Vector3>> = HashMap::new();
+        Self { trace_map }
+    }
+
+    pub fn add(&mut self, a: Vector3, b: Vector3) {
         Self::panic_if_ends_too_far_apart(&a, &b);
-        Self {}
+
+        self.trace_map.entry(a).or_default().insert(b);
+        self.trace_map.entry(b).or_default().insert(a);
+    }
+
+    pub fn get_graphs(&self) -> HashMap<i32, HashSet<Vector3>> {
+        let mut trace_id = 0;
+        let mut traces: HashMap<i32, HashSet<Vector3>> = HashMap::new();
+        let mut covered_points: HashSet<Vector3> = HashSet::new();
+
+        for (point, connections) in &self.trace_map {
+            if covered_points.contains(point) {
+                continue;
+            }
+
+            let mut points_on_trace: HashSet<Vector3> = HashSet::new();
+            self.find_all_points_on_trace(
+                &point,
+                &connections,
+                &mut points_on_trace,
+                &mut covered_points,
+            );
+
+            traces.insert(trace_id, points_on_trace.clone());
+            trace_id += 1;
+        }
+
+        return traces;
+    }
+
+    fn find_all_points_on_trace(
+        &self,
+        point: &Vector3,
+        connections: &HashSet<Vector3>,
+        points_on_trace: &mut HashSet<Vector3>,
+        covered_points: &mut HashSet<Vector3>,
+    ) {
+        if covered_points.contains(point) {
+            return;
+        }
+
+        points_on_trace.insert(*point);
+        covered_points.insert(*point);
+
+        for connected_point in connections {
+            let connections_at_point: &HashSet<Vector3> =
+                self.trace_map.get(connected_point).unwrap();
+
+            self.find_all_points_on_trace(
+                connected_point,
+                connections_at_point,
+                points_on_trace,
+                covered_points,
+            );
+        }
     }
 
     fn panic_if_ends_too_far_apart(a: &Vector3, b: &Vector3) {
@@ -17,24 +80,50 @@ impl TraceSegment {
             panic!("Trace ends too far apart! {} - {}", a, b);
         }
     }
-}
 
-pub struct TraceMap {
-    traces: Vec<TraceSegment>,
-}
+    /*
+    void FindAllPointsOnTrace(TraceSquare traceSquare, std::set<BoardPoint> &pointsOnTrace, std::set<BoardPoint> &coveredPoints) const
+        {
+            if (coveredPoints.find(traceSquare.position) != coveredPoints.end())
+            {
+                return; // This point already covered.
+            }
 
-impl TraceMap {
-    pub fn new() -> Self {
-        let traces = vec![];
-        Self { traces }
-    }
+            pointsOnTrace.insert(traceSquare.position);
+            coveredPoints.insert(traceSquare.position);
 
-    pub fn add(&mut self, a: Vector3, b: Vector3) {
-        self.traces.push(TraceSegment::new(a, b));
-    }
+            for (const BoardPoint &connection : traceSquare.connections)
+            {
+                const TraceSquare &connectedSquare = mTraceSquares.at(connection);
+                FindAllPointsOnTrace(connectedSquare, pointsOnTrace, coveredPoints); // Recurse on connections.
+            }
+        }
+    */
 
-    pub fn get_graphs(&self) -> Vec<i32> {
-        let list: Vec<i32> = self.traces.iter().map(|_f| 1).collect();
-        list
-    }
+    /*
+    std::map<int, std::set<BoardPoint>> GetTraces() const
+        {
+            int traceID = 0;
+            std::map<int, std::set<BoardPoint>> traces;
+
+            std::set<BoardPoint> coveredPoints;
+
+            for (const std::pair<BoardPoint, TraceSquare> &squarePair : mTraceSquares)
+            {
+                if (coveredPoints.find(squarePair.first) != coveredPoints.end())
+                {
+                    continue; // This point already covered in a previous trace.
+                }
+
+                std::set<BoardPoint> pointsOnTrace;
+
+                FindAllPointsOnTrace(squarePair.second, pointsOnTrace, coveredPoints);
+
+                traces.emplace(traceID, std::set<BoardPoint>(pointsOnTrace));
+                traceID++;
+            }
+
+            return traces;
+        }
+         */
 }
